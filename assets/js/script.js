71f1c71e9dda87155b92c267b531d9e5c12f9dec
Elise-16-medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2023-02-02 12:34:03 -0300

    commit portifolio

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+ScrollReveal({ 
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+ScrollReveal().reveal('.display-1', { delay: 800, origin: 'top', interval:100});
+ScrollReveal().reveal('.h3', { delay: 700, origin: 'top', interval:100});
+ScrollReveal().reveal('.list-inline', { delay: 600, origin: 'top', interval:100});
+ScrollReveal().reveal('#btn-contato', { delay: 500, origin: 'top', interval:100});
+ScrollReveal().reveal('.scroll-h2', { delay: 800, origin: 'top', interval:100});
+ScrollReveal().reveal('#img-title', { delay: 700, origin: 'left', interval:100});
+ScrollReveal().reveal('.scroll-p', { delay: 600, origin: 'bottom', interval:100});
+ScrollReveal().reveal('.skills-card', { delay: 800, origin: 'top', interval:100});
+ScrollReveal().reveal('.box', { delay: 700, origin: 'right', interval:400});
+ScrollReveal().reveal('.scroll-projetos', { delay: 800, origin: 'left', interval:100});
+ScrollReveal().reveal('.card', { delay: 700, origin: 'bottom', interval:300});
+ScrollReveal().reveal('.scroll-contato', { delay: 800, origin: 'top', interval:400});
+ScrollReveal().reveal('.list-unstyled', { delay: 700, origin: 'left', interval:200});
+ScrollReveal().reveal('.form-contato', { delay: 700, origin: 'right', interval:200});
+
+
+
+
+
